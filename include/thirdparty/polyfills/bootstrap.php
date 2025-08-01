fd2813505ce457f4794bcfd95e5b3edbdc877db6
gtbu <manbu@mail.de>
2025-08-01 10:41:23 +0200

    update polyfill loader

--- include/thirdparty/polyfills/bootstrap.php
+++ include/thirdparty/polyfills/bootstrap.php
@@ -6,6 +6,12 @@ require_once __DIR__.'/Mbstring/bootstrap.php';
 require_once __DIR__.'/Intl/bootstrap.php'; 
 require_once __DIR__.'/Uuid/bootstrap.php';
 
+if (\PHP_VERSION_ID < 70300) {
+    require_once __DIR__.'/Php73/bootstrap.php';
+}
+if (\PHP_VERSION_ID < 70400) {
+    require_once __DIR__.'/Php74/bootstrap.php';
+}
 if (\PHP_VERSION_ID < 80000) {
     require_once __DIR__.'/Php80/bootstrap.php';
 }
